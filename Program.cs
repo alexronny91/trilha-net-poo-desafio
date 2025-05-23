bcca58b94c4c80207731358e8df53ed335f76e9d
Alex Rufino <66045187+alexronny91@users.noreply.github.com>
2025-05-22 21:06:14 -0300

    Testes das classes iPhone e Nokia

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
-﻿using DesafioPOO.Models;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone iphone1 = new Iphone("11999999999", "Iphone 16", "imei123", 256);
+iphone1.Ligar();
+iphone1.ReceberLigacao();
+iphone1.InstalarAplicativo("Instagram");
+
+Nokia nokia1 = new Nokia("11988888888", "Nokia G21", "imei321", 128);
+nokia1.Ligar();
+nokia1.ReceberLigacao();
+nokia1.InstalarAplicativo("Facebook");
