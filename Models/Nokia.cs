9be659cfbd2850ac97ad5215f545df82842bba2c
Alex Rufino <66045187+alexronny91@users.noreply.github.com>
2025-05-22 21:11:48 -0300

    Implementação da classe Nokia de acordo com a classe abstrata Smartphone

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,17 @@
+using System.ComponentModel;
+
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} no dispositivo {Modelo}, com IMEI {IMEI} e memória RAM de {Memoria}gb");
+        }
     }
 }
